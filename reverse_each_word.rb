922a29e64df0e6df4964a155c95de9764e5887c6
Jack Cole <123jack.cole@gmail.com>
2020-01-18 02:29:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
-  regular_sentence = sentence.split("")
+  regular_sentence = sentence.split("").reverse
   reversed_sentence = []
   
   regular_sentence.each do |char|
-    reversed_sentence.push(char)
+    reversed_sentence.unshift(char)
   end
   
   p reversed_sentence.join("")
